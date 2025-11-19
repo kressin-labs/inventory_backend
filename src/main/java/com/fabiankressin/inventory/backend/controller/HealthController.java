4181c1510dcd9bf13ca49b4ae31058d3e98f7fdc
Fabian Kreßin <61588952+fabiankressin@users.noreply.github.com>
2025-11-19 22:37:22 +0100

    fix: nofix

--- src/main/java/com/fabiankressin/inventory/backend/controller/HealthController.java
+++ src/main/java/com/fabiankressin/inventory/backend/controller/HealthController.java
@@ -8,6 +8,6 @@ public class HealthController {
 
     @GetMapping("/api/health")
     public String health() {
-        return "Service is running and is reachable.";
+        return "Service is running and is accessible.";
     }
 }
